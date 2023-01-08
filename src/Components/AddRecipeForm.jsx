9fba0790ad42e979ae9266b4e0250f6100710472
Jessemwangi <jessejzee.com>
2023-01-09 01:12:35 +0200

    axios

--- src/Components/AddRecipeForm.jsx
+++ src/Components/AddRecipeForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import uuid from "react-uuid";
 import { Button, Container, UncontrolledTooltip } from "reactstrap";
