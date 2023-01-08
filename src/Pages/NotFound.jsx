fc1f78a756771aa7902e3ccb4ccae4f7b83de4c1
Jessemwangi <jessejzee.com>
2023-01-08 21:13:33 +0200

    home page contents

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <main>
             <h1>Page not Found</h1>
-
-            
+            <div>
+                <h4>Go back home</h4>
+                <Link to={'/'} className="btn btn-danger mt-3 mb-3 p-2 btn-lg" role="button">Home Page</Link>
+            </div>
         </main>
     );
 };
--- src/Views/HomeInfo.jsx
+++ src/Views/HomeInfo.jsx
@@ -18,13 +18,7 @@ const HomeInfo = () => {
                         <br />
                         <Link to={'/viewRecipes'} className="btn btn-danger mt-3 mb-3 p-2 btn-lg" role="button">View Recipes</Link>
                     </Col>
-                    <Col className='bg-light m-2 p-2 border border-danger d-grid'>
-                        <h2 className='noReview recipeAuthor bg-secondary rounded-top mb-3 p-4 ' style={{ color: "snow", textAlign: "left" }}>
-                            leave a Review</h2>
-                        Analyse recipes, see how different chefs use various ingridients to prepare meals that are tasty and unique, the nutrition  value it contains, each meal has the option to
-                        leave a review. The review is in form of ratings and comment, read what others have commentted.
-                        <Link to={'/'} className="btn btn-danger mt-3 mb-3 p-2 btn-lg" role="button">View Recipes</Link>
-                    </Col>
+
                     <Col className='bg-light m-2 p-2 border border-danger d-grid'>
                         <h2 className='noReview recipeAuthor bg-secondary rounded-top mb-3 p-4' style={{ color: "snow", textAlign: "left" }}>
                             Why Add it
@@ -34,12 +28,7 @@ const HomeInfo = () => {
                         and make cooking interesting
                         <Link to={'/addRecipe'} className="btn btn-danger mt-3 mb-3 p-2 btn-lg" role="button">post Your Recipe</Link>
                     </Col>
-                    <Col className='bg-light m-2 p-2 border border-danger d-grid'>
-                        <h2 className='noReview recipeAuthor bg-secondary rounded-top mb-3 p-4' style={{ color: "snow", textAlign: "left" }}>Multi Culture </h2>
-                        Get more from a diverse culture, prepare meals not only from local comunity but also get to see what other continents are
-                        cooking, gain knowledge, appreciate culture and share yours too. click link bellow to view posted recipe by various chefs
-                        <Link to={'/'} className="btn btn-danger mt-3 mb-3 p-2 btn-lg" role="button">View Recipes</Link>
-                    </Col>
+
                 </Row>
 
 
