511617012b1e6b7357b8153f6d2823d079cb65fd
Jessemwangi <jessejzee.com>
2023-01-09 21:12:18 +0200

    pop up notification

---
+++ src/Views/PopUpNotification.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+
+const PopUpNotification = ({notificationTitle,notificationMsg,showNotification}) => {
+    const [showNotification_,setShowNotification_]=useState(false);
+
+    // console.log('notificationTitle',notificationTitle,'notificationMsg',notificationMsg,'showNotification',showNotification)
+  useEffect(() => {
+if (showNotification){
+    setShowNotification_(true);
+}
+  },[showNotification]);
+
+    useEffect(() => {
+        const notification = () => {
+
+          if (showNotification_) {
+            setTimeout(() => setShowNotification_(false), 10000);
+          }
+    
+        }
+        notification();
+      }, [ showNotification_])
+
+    return (
+        <div>
+            {showNotification_ && 
+          <div className='popNotification' >
+          <h1 className='popNotification_title'>{notificationTitle}</h1>
+          <p className='popNotification_text'>{notificationMsg}</p>
+        </div>
+        }
+        </div>
+    );
+};
+
+export default PopUpNotification;
